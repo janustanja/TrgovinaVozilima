4a416370947729e428d458efca5051bed1558620
janustanja <janustanja@gmail.com>
2025-02-01 12:40:19 +0100

    Vozilo i VoziloController

--- Backend/Controllers/VoziloController.cs
+++ Backend/Controllers/VoziloController.cs
@@ -1,6 +1,125 @@
-﻿namespace Backend.Controllers
+﻿using Backend.Data;
+using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
 {
-    public class VoziloController
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class VoziloController: ControllerBase
     {
+        private readonly TrgovinaVozilimaContext _context;
+
+        public VoziloController(TrgovinaVozilimaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_context.Vozila);
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { poruka = e.Message });
+
+            }
+        }
+
+        [HttpGet]
+        [Route("{sifra:int}")]
+
+        public IActionResult GetBySifra(int sifra)
+        {
+            try
+            {
+                var s = _context.Vozila.Find(sifra);
+                if (s == null)
+                {
+                    return NotFound();
+                }
+                return Ok(s);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { poruka = e.Message });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(Vozilo vozilo)
+        {
+            try
+            {
+                _context.Vozila.Add(vozilo);
+                _context.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, vozilo);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { poruka = e.Message });
+            }
+
+        }
+
+        [HttpPut]
+        [Route("{sifra:int}")]
+        [Produces("application/json")]
+
+        public IActionResult Put(int sifra, Vozilo vozilo)
+        {
+            try
+            {
+
+                var s = _context.Vozila.Find(sifra);
+
+                if (s == null)
+                {
+                    return NotFound();
+                }
+
+                s.VrstaVozilaSifra = vozilo.VrstaVozilaSifra;
+                s.DobavljacSifra = vozilo.DobavljacSifra;
+                s.Marka = vozilo.Marka;
+                s.GodProizvodnje = vozilo.GodProizvodnje;
+                s.PrijedeniKm = vozilo.PrijedeniKm;
+                s.Cijena = vozilo.Cijena;
+                s.KupacSifra = vozilo.KupacSifra;
+
+
+                _context.Vozila.Update(s);
+                _context.SaveChanges();
+                return Ok(new { poruka = "Uspješno promijenjen podatak!" });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { poruka = e.Message });
+            }
+
+        }
+        [HttpDelete]
+        [Route("{sifra:int}")]
+        public IActionResult Delete(int sifra)
+        {
+            try
+            {
+                var s = _context.Vozila.Find(sifra);
+                if (s == null)
+                {
+                    return NotFound();
+                }
+                _context.Vozila.Remove(s);
+                _context.SaveChanges();
+                return Ok(new { poruka = "Uspješno obrisano!" });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { poruka = e.Message });
+            }
+        }
     }
 }
---
+++ Backend/Models/Vozilo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+namespace Backend.Models
+{
+    public class Vozilo : Entitet
+    {
+        public int DobavljacSifra { get; set; }
+        public int KupacSifra { get; set; }
+        public int VrstaVozilaSifra { get; set; }
+
+
+
+
+       [Column("VrstaVozilaSifra")]
+        public VrstaVozila VrstaVozila { get; set; }
+        [Column("DobavljacSifra")]
+        public Dobavljac Dobavljac { get; set; }
+        public string Marka { get; set; }
+        public string GodProizvodnje { get; set; }
+        public int PrijedeniKm { get; set; }
+        public decimal Cijena { get; set; }
+        [Column("KupacSifra")]
+        public Kupac Kupac { get; set; }
+    }
+}
