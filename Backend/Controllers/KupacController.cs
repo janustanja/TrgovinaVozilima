68c1d01f0a91c72e5f5735d1168aa7173812e737
janustanja <janustanja@gmail.com>
2025-02-01 12:40:01 +0100

    Kupac i KupacController

--- Backend/Controllers/KupacController.cs
+++ Backend/Controllers/KupacController.cs
@@ -1,6 +1,121 @@
-﻿namespace Backend.Controllers
+﻿using Backend.Data;
+using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
 {
-    public class KupacController
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class KupacController : ControllerBase
     {
+        private readonly TrgovinaVozilimaContext _context;
+
+        public KupacController(TrgovinaVozilimaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_context.Kupci);
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { poruka = e.Message });
+
+            }
+        }
+
+        [HttpGet]
+        [Route("{sifra:int}")]
+
+        public IActionResult GetBySifra(int sifra)
+        {
+            try
+            {
+                var s = _context.Kupci.Find(sifra);
+                if (s == null)
+                {
+                    return NotFound();
+                }
+                return Ok(s);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { poruka = e.Message });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(Kupac kupac)
+        {
+            try
+            {
+                _context.Kupci.Add(kupac);
+                _context.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, kupac);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { poruka = e.Message });
+            }
+
+        }
+
+        [HttpPut]
+        [Route("{sifra:int}")]
+        [Produces("application/json")]
+
+        public IActionResult Put(int sifra, Kupac kupac)
+        {
+            try
+            {
+
+                var s = _context.Kupci.Find(sifra);
+
+                if (s == null)
+                {
+                    return NotFound();
+                }
+
+                s.Ime = kupac.Ime;
+                s.Prezime = kupac.Prezime;
+                s.Adresa = kupac.Adresa;
+                s.Iban = kupac.Iban;
+
+                _context.Kupci.Update(s);
+                _context.SaveChanges();
+                return Ok(new { poruka = "Uspješno promijenjen podatak!" });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { poruka = e.Message });
+            }
+
+        }
+        [HttpDelete]
+        [Route("{sifra:int}")]
+        public IActionResult Delete(int sifra)
+        {
+            try
+            {
+                var s = _context.Kupci.Find(sifra);
+                if (s == null)
+                {
+                    return NotFound();
+                }
+                _context.Kupci.Remove(s);
+                _context.SaveChanges();
+                return Ok(new { poruka = "Uspješno obrisano!" });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { poruka = e.Message });
+            }
+        }
     }
 }
---
+++ Backend/Models/Kupac.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+namespace Backend.Models
+{
+    public class Kupac : Entitet
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Adresa { get; set; }
+        public string Iban { get; set; }
+    }
+}
