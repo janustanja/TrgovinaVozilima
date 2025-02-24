2d5be3db40f4737f43e3c80dee379d00240acbf5
janustanja <janustanja@gmail.com>
2025-02-24 18:42:27 +0100

    Rad na vanjskim kljucevima

--- Backend/Models/Vozilo.cs
+++ Backend/Models/Vozilo.cs
@@ -14,18 +14,18 @@ namespace Backend.Models
 
         [ForeignKey("dobavljac")]
         public required Dobavljac Dobavljac { get; set; }
-        
+
         //public int DobavljacSifra { get; set; }
 
-        public string Marka { get; set; }
-        public string GodProizvodnje { get; set; }
-        public int PrijedeniKm { get; set; }
+        public string Marka { get; set; } = "";
+        public string GodProizvodnje { get; set; } = "";
+        public int PrijedeniKm { get; set; } 
         public decimal Cijena { get; set; }
 
         //[Column("KupacSifra")]
 
         [ForeignKey("kupac")]
-        public required Kupac Kupac { get; set; }
+        public required Kupac Kupac { get; set; } 
         
         //public int KupacSifra { get; set; }
 
