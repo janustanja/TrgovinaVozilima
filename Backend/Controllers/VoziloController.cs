0fe54c59a5e7a89f203a8806d0af905d9bd9dbc7
edunovaProgramiranje <84087934+edunovaProgramiranje@users.noreply.github.com>
2025-02-26 18:01:41 +0100

    fix

--- Backend/Controllers/VoziloController.cs
+++ Backend/Controllers/VoziloController.cs
@@ -1,6 +1,7 @@
 ﻿using Backend.Data;
 using Backend.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Controllers
 {
@@ -20,7 +21,7 @@ namespace Backend.Controllers
         {
             try
             {
-                return Ok(_context.Vozila);
+                return Ok(_context.Vozila.Include(v=>v.Kupac).Include(v => v.VrstaVozila).Include(v => v.Dobavljac));
 
             }
             catch (Exception e)
